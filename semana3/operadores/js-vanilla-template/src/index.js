c916af6be1c135c3b650dc8a699185cc547d7727
hgalvao98 <henrique.galvao@hotmail.com>
2021-04-20 14:26:23 -0300

    js adicionado

---
+++ semana3/operadores/js-vanilla-template/src/index.js
@@ -0,0 +1,51 @@
+
+//Exercicios de interpretação
+
+//1. a.false b.false c.true d.boolean
+//2. a. undefined b. null c. 11 d. 3 e.[3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13] f.9
+
+//Exercicios de escrita
+//1
+// const suaIdade =  prompt('Qual é a sua idade?')
+// const amigoIdade = prompt('Qual é a idade de seu amigo?')
+
+// const condiçao = Number(suaIdade) > Number(amigoIdade)
+// const condiçao2 = Number(suaIdade) - Number(amigoIdade)
+
+// console.log('Sua idade é maior do que a do seu melhor amigo?', condiçao)
+// console.log('A diferença de idade é?', condiçao2)
+
+//2
+
+// const numeroPar = prompt ('Digite um numero par!')
+
+// console.log(Number(numeroPar)%2)
+
+//Todos os numeros pares vão resultar em 0. Se o usuario inserir um numero impar, seu restante irá aparecer.
+
+//3
+
+// const listaDeTarefas = [ ]
+
+// listaDeTarefas[0] = prompt ('digite uma atividade')
+// listaDeTarefas[1] = prompt ('digite uma outra atividade')
+// listaDeTarefas[2] = prompt ('digite uma ultima atividade')
+
+// console.log(listaDeTarefas)
+
+// let i = Number(prompt("Digite os numeros 0, 1 ou 2 para escolher a atividade ja feita"))
+
+// listaDeTarefas.splice(i, 1)
+
+// console.log(listaDeTarefas)
+
+//4
+
+// const email = prompt('Digite seu email:')
+// const nome = prompt ('Digite seu nome:')
+
+// console.log(`O e-mail ${email} foi cadastrado com sucesso. Seja bem-vindo(a), ${nome}`)
+
+
+
+
