f9a8f0e962b0c98292a6f949754b4693d2eb068f
hgalvao98 <henrique.galvao@hotmail.com>
2021-04-23 10:57:06 -0300

    javascript

---
+++ semana3/loops/js-vanilla-template/src/index.js
@@ -0,0 +1,54 @@
+//Interpretação
+
+//1 O codigo vai vai resultar em valores de 0 a 5 de cima para baixo.
+
+//2 a. No console vao ser impresso os numeros maiores que 18
+//2 b. Acredito que seria necessario colocar ''
+
+//Escrita
+
+//3
+
+//a.
+
+// const array = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+
+// for (let numero of array) {
+//     console.log(numero)
+// }
+
+//b.
+
+// for (let numero of array){
+//     console.log(numero / 10)
+// }
+
+//c.
+
+// for (let numero of array){
+//     if(numero%2 === 0){
+//     console.log(numero)
+//     }
+// }
+
+//d.
+
+
+//e.
+
+// const array = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+// let maiorNumero = 0
+// let menorNumero = 0
+
+// for (let i = 0; i < array.length; i++){
+//     if(array[i] > maiorNumero){
+//         maiorNumero = array[i]
+//     }
+// }
+// for (let i = 0; i < array.length; i++){
+//     if(array[i] < menorNumero){
+//         menorNumero = array[i]
+//     }
+// }
+
+// console.log('O maior numero e', maiorNumero, 'e o menor e', menorNumero)
