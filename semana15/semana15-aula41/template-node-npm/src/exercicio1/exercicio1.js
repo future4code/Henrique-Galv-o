539ba37d25e8875a90f644acede5e592df67c318
hgalvao98 <henrique.galvao@hotmail.com>
2021-07-12 15:49:26 -0300

    aula41

---
+++ semana15/semana15-aula41/template-node-npm/src/exercicio1/exercicio1.js
@@ -0,0 +1,5 @@
+const nome = process.argv[2]
+const idade = Number(process.argv[3])
+
+console.log(`Ola ${nome}, você tem ${idade} anos.`)
+console.log(`Ola ${nome}, voce tem ${idade}. Daqui 7 anos você vai ter ${idade + 7}`)
